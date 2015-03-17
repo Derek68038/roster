bbb97d881c2d1ec45cdf267f6c49a227d757c73c
Derek George <derek68038@gmail.com>
2015-03-17 11:17:53 -0500

    Created new rails project called 'roster'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roster::Application.config.secret_token = '36c05264f757e8a5416a7fa6265bbb91a4bc2794f8e23f3a8e883e57864d012442a2f6181a860e2242f2accb67c45f5d32eb8c32e7780a5f040f2dc53dedc6ed'

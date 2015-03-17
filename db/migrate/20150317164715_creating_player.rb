14bc7444cbb11f36f1db57ae9c216d0644609506
Derek George <derek68038@gmail.com>
2015-03-17 11:57:35 -0500

    Created schema file when migrating player db. Added Gem to fix an error in console.

--- db/migrate/20150317164715_creating_player.rb
+++ db/migrate/20150317164715_creating_player.rb
@@ -1,5 +1,10 @@
 class CreatingPlayer < ActiveRecord::Migration
   def change
-    
+    create_table :players do |t|
+      t.string :name
+      t.string :position
+      
+      t.timestamps
+    end
   end
 end

6213645bd106a24648a31b8f00647bd888375540
Derek George <derek68038@gmail.com>
2015-03-18 13:29:24 -0500

    Created new columns for player table

--- db/migrate/20150318182444_edit_player_table.rb
+++ db/migrate/20150318182444_edit_player_table.rb
@@ -1,7 +1,11 @@
 class EditPlayerTable < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    change_table :players do |t|
+      t.string :name
+      t.integer :number
+      t.integer :status_id
+      
+      t.timestamps
+    end
   end
 end

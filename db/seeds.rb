4dad8412f1388440a194d568139e1f47320e2bf4
Derek George <derek68038@gmail.com>
2015-04-07 14:48:13 -0500

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Position.create(category: 'QB')
+Position.create(category: 'RB')
+Position.create(category: 'WR')
+Position.create(category: 'TE')
+Position.create(category: 'FB')
+Position.create(category: 'DE')
+Position.create(category: 'OLB')
+Position.create(category: 'MLB')
+Position.create(category: 'CB')
+Position.create(category: 'S')
+
+Status.create(health: 'Healthy')
+Status.create(health: 'Probable')
+Status.create(health: 'Doubtful')
+Status.create(health: 'Injured Reserve')
